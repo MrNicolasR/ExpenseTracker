e9f9b8804e5c0982e27f1d2407ab0ca79f9cc6c4
nico <mr.n.rubert@gmail.com>
2022-07-09 18:48:23 -0400

    Created Basic Transaction Model

--- ExpenseTracker/Transactions/admin.py
+++ ExpenseTracker/Transactions/admin.py
@@ -1,3 +1,6 @@
+# Import
 from django.contrib import admin
+from .models import Transactions
 
 # Register your models here.
+admin.site.register(Transactions)
--- ExpenseTracker/Transactions/models.py
+++ ExpenseTracker/Transactions/models.py
@@ -1,3 +1,30 @@
+# Import 
 from django.db import models
 
-# Create your models here.
+
+# Category Choices
+categoryChoices = (
+    ('T', 'Transportation'),
+    ('H', 'Housing'),
+    ('F', 'Food'),
+    ('M', 'Miscellaneous'),
+)
+
+# Transactions Model
+class Transactions(models.Model):
+    # Transaction Name
+    name = models.CharField(max_length=100, null=False)
+    # Transaction Merchant
+    merchant = models.CharField(max_length=100, null=False)
+    # Transaction Amount
+    amount = models.FloatField(null=False)
+    # Transaction Discription
+    description = models.TextField(null=True)
+    # Tansaction Date
+    date = models.DateTimeField(auto_now=True)
+    # Transaction Category 
+    category = models.CharField(choices=categoryChoices,max_length=3, null=False)
+
+    def __str__():
+        return self.name + ' ' + self.amount.str()
+        
